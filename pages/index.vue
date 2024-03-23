07c57f169e9271b3e46956bc08427f13ad316344
mean-ji <winter7574@gmail.com>
2024-03-23 21:16:08 +0900

    feat: 소셜로그인 추가

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,22 @@
 
 <script lang="ts" setup>
 import { useAuthStore } from "~/store/auth";
+const common = mainCommon();
 const { userInfo } = storeToRefs(useAuthStore());
 const { logUserOut } = useAuthStore();
 const router = useRouter();
+const route = useRoute();
+
+onMounted(async () => {
+  await nextTick(() => {
+    // 소셜로그인 리턴 받았을 때
+    if (route.query.accessToken !== undefined) {
+      common.setCookie('token', route.query.accessToken, 8760);
+      common.goHome();
+    }
+  });
+});
+
 
 function login() {
   router.push("/login");
